b94a00360a6a510baf9935f684b1e1c6926443ce
Sultan <ss.softwareit@gmail.com>
2023-05-09 21:42:17 +0600

    binding

--- app/src/main/java/com/softwareit/xmlprojectpractice/MainActivity.kt
+++ app/src/main/java/com/softwareit/xmlprojectpractice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.softwareit.xmlprojectpractice
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.softwareit.xmlprojectpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
